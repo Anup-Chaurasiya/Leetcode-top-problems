a8325db9bde130084bf1c630ceffc9229044c801
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2024-08-10 04:26:46 +0530

    Time: 5 ms (82.08%) | Memory: 10 MB (9.29%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
 
-        if(s==t){
-            return true;
-        }
-        else{
-            return false;
-        }
+        // if(s==t){
+        //     return true;
+        // }
+        // else{
+        //     return false;
+        // }
 
-        
+
         // another method
-        /*
+        
         unordered_map<char,int>temp1;
         unordered_map<char,int>temp2;
         if(s.size()!=t.size()){
             return false;
         }
         for(int i=0;i<s.size();i++){
-            temp1[i]++;
-            temp2[i]++;
+            temp1[s[i]]++;
+            temp2[t[i]]++;
         }
-        if(temp1=temp2){
+        if(temp1==temp2){
             return true;
         }
         else{
             return false;
         }
-        */
+        
     }
 };
