3933cd5a76749b7bc3cb410c1936815092098b30
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2024-08-12 12:01:14 +0530

    Time: 99 ms (40.38%) | Memory: 75.6 MB (14.13%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        // for(int i=0;i<nums.size()-1;i++){
+        //     for(int j=i+1;j<nums.size();j++){
+        //         if(nums[i]==nums[j]){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+        unordered_map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        for(auto it:m){
+            if(it.second>1){
+                return true;
+            }
+        
+        }
+        return false;
+    }
+};
