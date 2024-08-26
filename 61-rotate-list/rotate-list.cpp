3fb286b93ba27367c139d6db1af952136bc9c232
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2024-08-26 15:08:54 +0530

    Time: 3 ms (85.22%) | Memory: 16.6 MB (32.14%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -1,44 +1,28 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL) return head;
+        if (head == NULL || head->next == NULL || k == 0) return head;
 
-        ListNode*temp=head;
-        int size=0;
-        while(temp){
+        ListNode* tail = head;
+        int size = 1;
+        while (tail->next) {
+            tail = tail->next;
             size++;
-            temp=temp->next;
         }
 
-        int loop=k % size;
-        if(size==1 || loop==0) return head;
+        k = k % size;
+        if (k == 0) return head;
 
-        loop = size - loop ;
-        temp=head;
-        int count=0;
-        while(count<loop-1){
-            temp=temp->next;
-            count++;
+        ListNode* newTail = head;
+        for (int i = 1; i < size - k; i++) {
+            newTail = newTail->next;
         }
-        ListNode *rotate=temp->next;
-        temp->next=NULL;
-        ListNode*first=rotate;
-        while(rotate->next!=NULL){
-            rotate=rotate->next;
-        }
-        rotate->next=head;
 
-        return first;
+        ListNode* newHead = newTail->next;
+        newTail->next = NULL;
+        tail->next = head;
+        return newHead;
 
     }
 };
+
