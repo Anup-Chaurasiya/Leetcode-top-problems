b1804ed6ac873479c3e8317102debe98eb513c31
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2024-08-31 17:24:38 +0530

    Time: 46 ms (35.02%) | Memory: 58.9 MB (48.63%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -3,7 +3,6 @@ public:
     int longestSubarray(vector<int>& nums) {
           int i=0;
         int j=-1;
-        int coziw=0;
         int result=0;
         int count=0;
         while(i<nums.size()){
