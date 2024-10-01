28a48a79593540c158e092a6337d1ea2adfe0503
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2024-10-01 14:35:15 +0530

    Time: 8 ms (77.78%) | Memory: 14.4 MB (21.24%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,35 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<pair<int, int>> indexedNums;  // Pair of (value, index)
-        
-        // Store original indices
-        for (int i = 0; i < nums.size(); i++) {
-            indexedNums.push_back({nums[i], i});
+        unordered_map<int,int > mp;
+        int n=nums.size(); 
+        for(int i=0;i<n;i++){
+            int rem = target - nums[i];
+            if(mp.find(rem) != mp.end()){
+                return {mp[rem],i};
+            } 
+
+            mp[nums[i]] = i;
         }
-        
-        // Sort based on the values
-        sort(indexedNums.begin(), indexedNums.end());
-        
-        // Two-pointer approach
-        int s = 0, e = nums.size() - 1;
-        vector<int> ans;
-        
-        while (s < e) {
-            int sum = indexedNums[s].first + indexedNums[e].first;
-            if (sum == target) {
-                ans.push_back(indexedNums[s].second);
-                ans.push_back(indexedNums[e].second);
-                break;
-            }
-            else if (sum > target) {
-                e--;
-            }
-            else {
-                s++;
-            }
-        }
-        
-        return ans;
+        return {};
     }
 };
