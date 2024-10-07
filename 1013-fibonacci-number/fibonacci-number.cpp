5b62897b9e1c1a3a16eeb3383b6fa72bec82218f
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2024-10-07 12:30:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (34.16%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -13,10 +13,24 @@ int fibDp(int n,vector<int>&dp){
     return dp[n] = fibDp(n-1,dp) + fibDp(n-2,dp);
 }
 
+int fibTail(int n,vector<int>&dp){
+    if(n<=1) return n;
+    dp[0]=0;
+    dp[1]=1;
+
+    for(int i=2;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    return dp[n];
+}
+
 
     int fib(int n) {
-        vector<int>dp(n+1,-1);
-        return fibDp(n,dp);
+        vector<int>dp(n+1);
+        // return fibDp(n,dp);
+
+        return fibTail(n,dp);
 
     }
 };
