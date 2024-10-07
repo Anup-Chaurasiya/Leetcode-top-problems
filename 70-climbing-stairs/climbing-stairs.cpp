4ae760b5e3d93bf235caf48290947817ebb8f35c
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2024-10-07 12:58:29 +0530

    Time: 1 ms (48.90%) | Memory: 7.6 MB (37.29%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -13,10 +13,27 @@ int climbingDp(int n,vector<int>&dp){
     return dp[n] = climbingDp(n-1,dp) + climbingDp(n-2,dp); 
 }
 
+int climbTailDp(int n ,vector<int>&dp){
+    
+    if(n<=2){
+        return n;
+    }
+
+    dp[1]=1;
+    dp[2]=2;
+
+    for(int i=3;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    return dp[n];
+}
 
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
 
-        return climbingDp(n,dp);
+        // return climbingDp(n,dp);
+
+        return climbTailDp(n,dp);
     }
 };
