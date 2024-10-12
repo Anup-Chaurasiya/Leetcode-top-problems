57a2d5eaf01205b707a923082a2289250991cb19
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2024-10-12 22:36:24 +0530

    Time: 3 ms (94.38%) | Memory: 10.9 MB (86.05%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,22 +1,33 @@
 class Solution {
+
 private:
-    int nums(char n) {
-        switch (n) {
-            case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
-            case 'M': return 1000;
-            default: return 0; 
+    int nums(char ch) {
+
+        switch (ch) {
+        case 'I':
+            return 1;
+        case 'V':
+            return 5;
+        case 'X':
+            return 10;
+        case 'L':
+            return 50;
+        case 'C':
+            return 100;
+        case 'D':
+            return 500;
+        case 'M':
+            return 1000;
+        default:
+            return 0;
         }
-    }   
+    }
+
 public:
     int romanToInt(string s) {
         int sum = 0;
         int n = s.size();
-        for (int i = 0; i < n - 1; ++i) {
+        for (int i = 0; i < n - 1; i++) {
             if (nums(s[i]) < nums(s[i + 1])) {
                 sum -= nums(s[i]);
             } else {
