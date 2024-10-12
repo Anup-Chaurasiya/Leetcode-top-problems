2daf22d7e1b70dce4f0407548f0b466f834bf48f
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2024-10-12 23:32:29 +0530

    Time: 4 ms (79.79%) | Memory: 8.9 MB (53.56%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         int start=0,end=s.size()-1;
-        while(start<end){
+        while(start<=end){
             if(!isalnum(s[start])){
                 start++;
             }
@@ -18,6 +18,7 @@ public:
             }
 
         }
+
         return true;
     }
 };
