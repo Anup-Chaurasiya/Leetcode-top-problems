377969c5d4f5e38fbb891f7ba72ba57342ac69bd
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2024-10-13 18:08:47 +0530

    Time: 3 ms (43.48%) | Memory: 7.8 MB (67.73%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,30 +1,26 @@
-#include <string>
-using namespace std;
-
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        string r1 = "", r2 = "";
-
-        // Process the first string
-        for (char ch : s) {
-            if (ch >= 'a' && ch <= 'z') {
+        string r1="";
+        string r2="";
+       for(char ch : s){
+        if(ch>='a' && ch<='z'){
                 r1 += ch;
-            } else if (ch == '#' && !r1.empty()) {
+            }
+            else if(ch=='#' && !r1.empty()){
                 r1.pop_back();
             }
-        }
+       }
 
-        // Process the second string
-        for (char ch : t) {
-            if (ch >= 'a' && ch <= 'z') {
-                r2 += ch;
-            } else if (ch == '#' && !r2.empty()) {
+        for(char ct : t){
+            if(ct>='a' && ct<='z'){
+                r2 += ct;
+            }
+            else if(ct=='#' && !r2.empty()){
                 r2.pop_back();
             }
         }
 
-        
-        return r1 == r2;
+        return r1==r2;
     }
 };
