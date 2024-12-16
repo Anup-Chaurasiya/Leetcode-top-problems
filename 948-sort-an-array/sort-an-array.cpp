fb4ae781d7af37ef0bf9ed8393adefa7a5eab6cf
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2024-12-16 19:00:01 +0530

    Time: 79 ms (62.00%) | Memory: 96.5 MB (48.36%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,36 +1,63 @@
 class Solution {
 public:
 
-void MinHeapify(vector<int> &arr, int N, int i) {
-    int largest = i; // 
-    int left = 2 * i + 1;
-    int right = 2 * i + 2; 
+// // ********** Heap sort *********
 
-    if (left < N && arr[left] > arr[largest])
-        largest = left;
+// void MaxHeapify(vector<int> &arr, int N, int i) {
+//     int largest = i; // 
+//     int left = 2 * i + 1;
+//     int right = 2 * i + 2; 
 
-    if (right < N && arr[right] > arr[largest])
-        largest = right;
+//     if (left < N && arr[left] > arr[largest])
+//         largest = left;
 
-    if (largest != i) {
-        swap(arr[i], arr[largest]);
+//     if (right < N && arr[right] > arr[largest])
+//         largest = right;
+
+//     if (largest != i) {
+//         swap(arr[i], arr[largest]);
         
-        MinHeapify(arr, N, largest);
-    }
-}
+//         MaxHeapify(arr, N, largest);
+//     }
+// }
 
-    vector<int> sortArray(vector<int>& nums) {
-        int size=nums.size();
+//     vector<int> sortArray(vector<int>& nums) {
+//         int size=nums.size();
 
-        for(int i=size/2-1;i>=0;i--){
-            MinHeapify(nums, size, i);
-        }
+//         for(int i=size/2-1;i>=0;i--){
+//             MaxHeapify(nums, size, i);
+//         }
 
-        while(size>0){
-            swap(nums[0],nums[size-1]);
-            size--;
-            MinHeapify(nums, size, 0);
-        }
-        return nums;
+//         while(size>0){
+//             swap(nums[0],nums[size-1]);
+//             size--;
+//             MinHeapify(nums, size, 0);
+//         }
+//         return nums;
+//     }
+// };
+
+// ********* Counting Sort **********
+vector<int> sortArray(vector<int>& nums){
+       unordered_map<int,int>mpp;
+
+       int minValue = INT_MAX; 
+       int maxValue = INT_MIN; 
+       for(auto i : nums){
+           mpp[i]++;
+           minValue = min(minValue,i);
+           maxValue = max(maxValue,i);
+       }
+       
+       int index = 0;
+       for(int i = minValue;i<=maxValue;i++){
+           if(mpp.find(i) != mpp.end()){
+               int count = mpp[i];
+               while(count--){
+                   nums[index++] = i ;
+               }
+           }
+       }
+       return nums;
     }
 };
