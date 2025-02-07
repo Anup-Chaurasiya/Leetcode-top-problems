83c8d8474789a333aff0c023efc74b3fd37c215e
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2025-02-07 16:29:32 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (93.79%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        if (height.empty()) return 0; // Handle edge case for empty input
-        
-        int leftmost = height[0], rightmost = height[height.size() - 1];
-        int ans = 0, index = 0;
-        int n = height.size();
-
-        // Find the index of the highest bar
-        int maxheight = height[0];
-        for (int i = 1; i < n; i++) {
-            if (height[i] > maxheight) {
-                maxheight = height[i];
-                index = i;
+        if (height.empty()) return 0;
+        int n = height.size(); 
+        int maxi  = 0;
+        int index =0 ;
+        int leftmost = height[0];
+        int rightmost =height[n-1] ;
+        int ans =0;
+        for(int i=0;i<n;i++){
+            if(height[i]>maxi){
+                maxi = height[i];
+                index = i ; 
             }
         }
-
-        // Calculate trapped water from left to highest bar
-        int i = 1;
+         int i = 1;
         while (i < index) {
             if (leftmost > height[i]) {
                 ans += (leftmost - height[i]);
@@ -26,8 +23,6 @@ public:
             }
             i++;
         }
-
-        // Calculate trapped water from right to highest bar
         int e = n - 2;
         while (e > index) {
             if (rightmost > height[e]) {
