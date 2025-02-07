468088bb4f67b270798622165c2b3cbaf7a02518
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2025-02-07 11:35:16 +0530

    Time: 46 ms (75.18%) | Memory: 28.9 MB (97.12%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
         vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            int a=nums[i];
-            int t =-a;
-            int s=i+1,e=nums.size()-1;
+        for(int i=0;i<n;i++){
+            int t = -(nums[i]);
+            int s = i+1 ;
+            int e = n-1;
             while(s<e){
-                if(nums[s]+nums[e]==t){
+                if(t==nums[s]+nums[e]){
                     ans.push_back({nums[i],nums[s],nums[e]});
-                    while(s<e && nums[s]==nums[s+1])s++;
-                    while(e>s && nums[e]==nums[e-1])e--;
-                     s++;
-                     e--;
+                    while(s<e && nums[s] == nums[s+1]) s++;
+                    while(e>s && nums[e] == nums[e-1]) e--;
+                    s++;
+                    e--;
                 }
-                 else if(nums[s]+nums[e]>t){
-                e--;
+                else if(nums[s]+nums[e]<t){
+                    s++;
                 }
-                 else{
-                s++;
+                else{
+                    e--;
                 }
+
             }
-            while(i+1<nums.size() && nums[i]==nums[i+1])
-                i++;
+            while(i+1<n && nums[i]==nums[i+1]) i++;
         }
         return ans;
     }
