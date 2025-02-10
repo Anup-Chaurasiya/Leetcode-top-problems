a638317386a686252344edaaedb78106d57aabb7
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2025-02-10 19:09:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (52.94%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,47 +1,51 @@
 class Solution {
+private:
+    int findindex1(vector<int>&nums,int target){
+         int start = 0;
+        int end = nums.size()-1;
+        int left = -1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+
+            if(nums[mid]==target){
+                left = mid ;
+                end = mid -1;
+            }
+            else if(nums[mid]<target){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return left;
+
+    }
+    int findindex2(vector<int>&nums,int target){
+        int start = 0;
+        int end = nums.size()-1;
+        int right = -1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+
+            if(nums[mid]==target){
+                right = mid ;
+                start = mid + 1;
+            }
+            else if(nums[mid]<target){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return right;
+    }
 public:
-
-int lowerBound(vector<int>& nums, int target){
-     int size=nums.size();
-        int mid,start=0,end=size-1;
-        int first=-1,last=-1;
-        while(start<=end){
-            mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                first = mid;
-                end=mid-1;
-            }
-            else if(nums[mid]<target){
-                start=mid+1;
-            }
-            else
-            end=mid-1;
-        }
-        return first;
-}
-
-int upperBound(vector<int>& nums, int target){
-     int size=nums.size();
-        int mid,start=0,end=size-1;
-        int first=-1,last=-1;
-        while(start<=end){
-            mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                last = mid;
-                start=mid+1;
-            }
-            else if(nums[mid]<target){
-                start=mid+1;
-            }
-            else
-            end=mid-1;
-        }
-    return last;
-}
-
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first=lowerBound(nums,target);
-        int second = upperBound(nums,target);
-        return {first,second};
+      int left = findindex1(nums,target);
+      int right = findindex2(nums,target);
+
+      return {left,right};
     }
 };
