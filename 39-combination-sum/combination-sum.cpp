c1f728620608286cdb8cf983665a43bdfaed7937
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2025-02-16 14:33:34 +0530

    Time: 732 ms (5.01%) | Memory: 431 MB (5.14%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    void findCombination(int id, int target, vector<int>& arr, vector<vector<int>>& ans, vector<int>& dp) {
-        if (target == 0) {
-            ans.push_back(dp);
-            return;
-        }
+    set<vector<int>>s;
+    void combination(int id, vector<int>&arr, int target, vector<int>comb, vector<vector<int>> & ans){
         
-        if (id == arr.size() || target < 0) {
-            return;
+        if(id==arr.size() || target < 0){
+            return ;
+        }
+        if(target==0){
+            if(s.find(comb)== s.end())
+            ans.push_back(comb);
+            s.insert(comb);
+            return; 
         }
 
-        // Take the current element
-        if (arr[id] <= target) {
-            dp.push_back(arr[id]);
-            findCombination(id, target - arr[id], arr, ans, dp);
-            dp.pop_back();  // Backtrack
-        }
-
-        // Skip the current element
-        findCombination(id + 1, target, arr, ans, dp);
+        comb.push_back(arr[id]);
+        combination(id+1,arr,target-arr[id],comb,ans); // single inclusion
+        combination(id,arr,target-arr[id],comb,ans);   // multiple inclusion
+        comb.pop_back();   // backtracking 
+        combination(id+1,arr,target,comb,ans);
     }
-
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> dp;
-        findCombination(0, target, candidates, ans, dp);
+        vector<vector<int>>ans;
+        vector<int> comb;
+
+        combination(0,candidates,target,comb,ans);
         return ans;
     }
 };
