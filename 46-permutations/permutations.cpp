c5523a655aa5f0f06721930125b100ae6b814173
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2025-02-19 19:50:52 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (21.50%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,59 +1,61 @@
-// class Solution {
-
-//     private:
-
-//     void permutation(vector<int> nums, vector<vector<int>> &ans,int index){
-//         if(index>=nums.size()){
-//             ans.push_back(nums);
-//             return ;
-//         }
-//         for(int i=index;i<nums.size();i++){
-//             swap(nums[index],nums[i]);
-//             permutation(nums,ans,index+1);
-//             // backtracking
-//             swap(nums[index],nums[i]);
-//         }
-//     }
-
-// public:
-//     vector<vector<int>> permute(vector<int>& nums) {
-//         int index=0;
-//         vector<vector<int>>ans;
-//         vector<int>temp;
-//         permutation(nums,ans,index);
-
-//         return ans;
-//     }
-// };
-
 class Solution {
 
     private:
 
-    void permutation(vector<int> nums, vector<vector<int>> &ans,vector<int>&temp,vector<bool>visited){
-        if(temp.size()==visited.size()){
-            ans.push_back(temp);
+    void permutation(vector<int> nums, vector<vector<int>> &ans,int index){
+        if(index==nums.size()){
+            ans.push_back(nums);
             return ;
         }
-        for(int i=0;i<nums.size();i++){
-           if(visited[i]==0){
-            visited[i]=1;
-            temp.push_back(nums[i]);
-            permutation(nums,ans,temp,visited);
-            visited[i]=0;
-            temp.pop_back();
-
-           }
+        for(int i=index;i<nums.size();i++){
+            swap(nums[index],nums[i]);
+            permutation(nums,ans,index+1);
+            // backtracking
+            swap(nums[index],nums[i]);
         }
     }
 
 public:
     vector<vector<int>> permute(vector<int>& nums) {
+        int index=0;
         vector<vector<int>>ans;
         vector<int>temp;
-        vector<bool>visited(nums.size(),0);
-        permutation(nums,ans,temp,visited);
+        permutation(nums,ans,index);
 
         return ans;
     }
 };
+
+// second approach
+
+// class Solution {
+
+//     private:
+
+//     void permutation(vector<int> nums, vector<vector<int>> &ans,vector<int>&temp,vector<bool>visited){
+//         if(temp.size()==visited.size()){
+//             ans.push_back(temp);
+//             return ;
+//         }
+//         for(int i=0;i<nums.size();i++){
+//            if(visited[i]==0){
+//             visited[i]=1;
+//             temp.push_back(nums[i]);
+//             permutation(nums,ans,temp,visited);
+//             visited[i]=0;
+//             temp.pop_back();
+
+//            }
+//         }
+//     }
+
+// public:
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>>ans;
+//         vector<int>temp;
+//         vector<bool>visited(nums.size(),0);
+//         permutation(nums,ans,temp,visited);
+
+//         return ans;
+//     }
+// };
