e87cd5fb76e3484f3d84ee8ecbfa1f8f8a5cb45c
Anup-Chaurasiya <118820877+Anup-Chaurasiya@users.noreply.github.com>
2025-09-26 10:27:00 +0530

    Time: 21 ms (6.27%) | Memory: 31.9 MB (81.99%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -1,26 +1,8 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        unordered_map<int,int>mpp;
-
-       int minValue = INT_MAX; 
-       int maxValue = INT_MIN; 
-       for(auto i : nums){
-           mpp[i]++;
-           minValue = min(minValue,i);
-           maxValue = max(maxValue,i);
-        }
-       int index = 0;
-       for(int i = minValue;i<=maxValue;i++){
-           if(mpp.find(i) != mpp.end()){
-               int count = mpp[i];
-               while(count--){
-                   nums[index++] = i ;
-               }
-           }
-       }
-
-       int sum = 0 ;
+        sort(nums.begin(),nums.end());
+        int sum = 0 ;
        for(int i=0;i<nums.size();i=i+2){
             sum += nums[i]; 
         }
